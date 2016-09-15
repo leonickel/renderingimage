57154f5e314c3271f4efc8ddaade8453fd02dbf2
Leonardo Nickel <lnicke2@L-BRZN6HLG3Q-M.local>
2016-09-14 23:42:26 -0300

    exception

---
+++ src/main/java/com/leonickel/renderingimage/exception/NoVideoFoundException.java
@@ -0,0 +1,13 @@
+package com.leonickel.renderingimage.exception;
+
+public class NoVideoFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3692569596463375264L;
+
+	public NoVideoFoundException(String message) {
+		super(message);
+	}
+}
